ea2576c95a7d9c550a7cda6ab2c30382d5a78ab5
Sore <soren@sorenbaird.com>
2023-12-19 09:24:47 -0600

    Changed flex to float, added localStorage logic

--- components/NotePad/NotePad.js
+++ components/NotePad/NotePad.js
@@ -1,6 +1,34 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './NotePad.module.scss';
 
 const NotePad = ({ sideRailOpen }) => {
+  const timeoutRef = useRef(null);
+  const [textValue, setTextValue] = useState('');
+
+  useEffect(() => {
+    const files = JSON.parse(localStorage.getItem('simpleWriterFiles') || {});
+    if (files['Test Document']) {
+      setTextValue(files['Test Document']);
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    setTextValue(e.target.value);
+    clearTimeout(timeoutRef.current);
+    // TODO: If I use a setTimeout, it's going to set the value when the timeout is created,
+    // meaning the text value will never be up to date.
+    timeoutRef.current = setTimeout(() => {
+      console.log('[SOREN save file]');
+
+      const files = JSON.parse(localStorage.getItem('simpleWriterFiles') || '{}');
+
+      files['Test Document'] = textValue;
+      
+      localStorage.setItem('simpleWriterFiles', JSON.stringify(files));
+    }, 2000);
+  }
+
+  console.log('[SOREN text value]', textValue);
 
   return (
     <div
@@ -10,6 +38,8 @@ const NotePad = ({ sideRailOpen }) => {
       <textarea
         className={styles.textarea}
         placeholder="Once upon a time..."
+        onChange={(e) => handleChange(e)}
+        value={textValue}
       />
     </div>
   );
